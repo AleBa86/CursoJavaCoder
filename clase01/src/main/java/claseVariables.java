d3a49ee19a2bc421f2f2b31790fcc69a601ca292
unknown <alejandrog.baez@gmail.com>
2024-05-23 14:11:35 -0300

    primer commit

---
+++ clase01/src/main/java/claseVariables.java
@@ -0,0 +1,10 @@
+public class claseVariables {
+
+    public static void main(String[] args){
+        /*Así se comenta
+        * multilinea*/
+        //comentario de una línea
+        
+        System.out.println("Hello");
+    }
+}
